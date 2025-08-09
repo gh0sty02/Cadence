ea694940b371e4f60a1c0b8e546a1bbcc32da426
gh0sty02 <pranayyadav08@gmail.com>
2025-08-10 02:57:08 +0530

    fix: server build errors

--- apps/server/src/routes/index.ts
+++ apps/server/src/routes/index.ts
@@ -1,13 +1,9 @@
 import { Router } from "express";
 
-import testRouter from "./test";
-
-const router = Router();
+const router: Router = Router();
 
 router.get("/", function (_req, res) {
   res.send("Express API is running");
 });
-
-router.use("/test", testRouter);
 
 export default router;
